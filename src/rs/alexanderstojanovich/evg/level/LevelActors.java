347e332174b9b8621bc1ac1b33041e79dbb1a7e6
Aleksandar Stojanovic <coas91@rocketmail.com>
2024-10-22 05:41:06 +0200

    Build v51. Client/Server. Fix not rendering weapons on other players.

--- src/rs/alexanderstojanovich/evg/level/LevelActors.java
+++ src/rs/alexanderstojanovich/evg/level/LevelActors.java
@@ -194,17 +194,19 @@ public class LevelActors {
                             pi.uniqueId,
                             new Model(levelContainer.gameObject.GameAssets.ALEX_BODY_DEFAULT)
                     );
-                    IList<WeaponIfc> weaponsAsList = GapList.create(Arrays.asList(levelContainer.weapons.AllWeapons));
-                    WeaponIfc weapon = weaponsAsList.getIf(w -> w.getTexName().equals(pi.weapon));
-                    if (weapon == null) { // if there is no weapon, switch to 'NONE' - unarmed, avoid nulls!
-                        weapon = Weapons.NONE;
-                    }
-                    opOrNull.switchWeapon(weapon);
                     otherPlayers.add(opOrNull);
                 }
                 opOrNull.setName(pi.name);
                 opOrNull.body.setPrimaryRGBAColor(pi.color);
-                opOrNull.body.setTexName(pi.texModel);
+//                opOrNull.body.setTexName(pi.texModel);
+
+                IList<WeaponIfc> weaponsAsList = GapList.create(Arrays.asList(levelContainer.weapons.AllWeapons));
+                WeaponIfc weapon = weaponsAsList.getIf(w -> w.getTexName().equals(pi.weapon));
+                if (weapon == null) { // if there is no weapon, switch to 'NONE' - unarmed, avoid nulls!
+                    weapon = Weapons.NONE;
+                }
+                opOrNull.switchWeapon(weapon);
+                opOrNull.switchBodyModel();
             }
         });
     }
